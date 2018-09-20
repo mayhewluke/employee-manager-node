49b3dae17614ad800b4b02db7ad002902160170c
Luke Mayhew <luke@webuildlegends.com>
2018-09-20 13:44:59 -0600

    Add `secrets` directory... for secrets (eg. service credentials)

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
   preset: "ts-jest",
   testMatch: ["<rootDir>/test/**/*.(j|t)s?(x)"],
   testEnvironment: "node",
+  moduleNameMapper: {
+    "^secrets/(.*)": "<rootDir>/secrets/$1"
+  },
+  moduleDirectories: ["src", "node_modules"],
   watchPathIgnorePatterns: ["<rootDir>/dist"]
 };

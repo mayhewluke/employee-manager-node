5b6ee31fb789613e6b001dee12c4fca76e4e8817
Luke Mayhew <luke@webuildlegends.com>
2018-09-20 11:46:55 -0600

    Allow port to be set via PORT env var

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from "express";
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/", (_, res) => res.send("Hello World!"));
 
 /* tslint:disable:no-console */
 /* eslint-disable-next-line no-console */
-app.listen(3000, () => console.log("Example app listening on port 3000"));
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
 /* tslint:enable:no-console */

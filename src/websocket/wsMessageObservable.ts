35a892c09dbae1de500300afcbcb437387f8e3f9
Luke Mayhew <luke@webuildlegends.com>
2018-09-22 10:04:05 -0600

    Proper typing for wsMessageObservable

--- src/websocket/wsMessageObservable.ts
+++ src/websocket/wsMessageObservable.ts
@@ -1,10 +1,16 @@
-import { fromEventPattern } from "rxjs";
+import { Observable, Observer, TeardownLogic } from "rxjs";
 
 import WebSocket = require("ws");
 
-// TODO type safety for `handler`, and return type
-export default (ws: WebSocket) =>
-  fromEventPattern(
-    (handler: any) => ws.on("message", handler),
-    (handler: any) => ws.off("message", handler)
+type Handler = (this: WebSocket, data: WebSocket.Data) => void;
+
+// The higher-level observable creation methods don't work properly for one
+// reason or another, so low-level `create` has to be used
+export default (ws: WebSocket): Observable<WebSocket.Data> =>
+  Observable.create(
+    (observer: Observer<WebSocket.Data>): TeardownLogic => {
+      const handler: Handler = x => observer.next(x);
+      ws.on("message", handler);
+      return () => ws.off("message", handler);
+    }
   );
